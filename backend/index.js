e0e7e1e0ab1457870862655dc8dd6fca55bf47a1
mukeshlakkakula <lakkakulababblu@gmail.com>
2024-08-08 20:46:08 +0530

    changed db

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,7 @@ app.use(cors());
 const mongoDB = require("./db");
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://cloud-kitchenfullone-frontend.vercel.app"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
