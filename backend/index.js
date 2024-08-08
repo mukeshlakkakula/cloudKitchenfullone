5d669bee51adfef1e3486c912b7644c94f8839b3
Mukesh <139950191+mukeshlakkakula@users.noreply.github.com>
2024-08-08 13:22:56 +0530

    Update index.js
    conflict for

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const mongoDB = require("./db");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin","*");
+ 
   res.header(
     "Access-Control-Allow-Headers",
     "Origin,X-Requested-With,Content-Type,Accept"
